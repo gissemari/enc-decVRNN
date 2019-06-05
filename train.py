082fd87a71001557609f1910cc4855ed80d7e182
gissemari <gissemari@gmail.com>
2019-06-05 15:34:22 -0400

    saving log

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import csv
 
 import numpy as np
 import torch as t
@@ -10,7 +11,12 @@ from utils.batch_loader import BatchLoader
 from utils.parameters import Parameters
 from model.rvae import RVAE
 
-
+def writeColumns(filename, zipList):
+    with open(filename, "w") as f:
+        writer = csv.writer(f)
+        for row in zipList:
+            writer.writerow(row) # for more writerow
+    return
 
 if __name__ == "__main__":
 
@@ -59,6 +65,7 @@ if __name__ == "__main__":
 
     ce_result = []
     kld_result = []
+    nll_local_result = []
 
     scheduler = MultiStepLR(optimizer, milestones=[100], gamma=0.1)
     for iteration in range(args.num_iterations):
@@ -110,10 +117,10 @@ if __name__ == "__main__":
 
             print('Validation-it: {}, loss-val: {:.4f}, cross_entropy: {:.4f}, KL: {:.4f}, coef {:8f}'.format(iteration, lossVal, cross_entropyVal/args.batch_size, kldVal/args.batch_size,coef))
         '''
-        if iteration % 10 == 0:
+        if iteration % (args.num_iterations/10) == 0:
             ce_result += [cross_entropy]
             kld_result += [kld]
-
+            nll_local_result +=[nll_local]
             seed = np.random.normal(size=[1, parameters.latent_variable_size])
             #sample = rvae.sample(batch_loader, 50, seed, args.use_cuda)
             #print('------------SAMPLE------------')
@@ -123,3 +130,4 @@ if __name__ == "__main__":
 
     np.save('output/ce_result_{}.npy'.format(args.ce_result), np.array(ce_result))
     np.save('output/kld_result_npy_{}'.format(args.kld_result), np.array(kld_result))
+    writeColumns('output/log.csv',zip(ce_result,kld_result))
